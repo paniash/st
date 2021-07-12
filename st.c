173d97f331bc435e5c2aa46121be49215f3d416a
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-07-12 14:01:10 +0530

    added compatibility with swallow

--- st.c
+++ st.c
@@ -1087,14 +1087,25 @@ tswapscreen(void)
 void
 newterm(const Arg* a)
 {
+	int res;
 	switch (fork()) {
 	case -1:
 		die("fork failed: %s\n", strerror(errno));
 		break;
 	case 0:
-		chdir(getcwd_by_pid(pid));
-		execlp("st", "./st", NULL);
-		break;
+		switch (fork()) {
+		case -1:
+			die("fork failed: %s\n", strerror(errno));
+			break;
+		case 0:
+			res = chdir(getcwd_by_pid(pid));
+			execlp("st", "./st", NULL);
+			break;
+		default:
+			exit(0);
+		}
+	default:
+		wait(NULL);
 	}
 }
 
